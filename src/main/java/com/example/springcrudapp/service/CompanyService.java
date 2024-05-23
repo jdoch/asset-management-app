0b00a01cf199e0bcc38b51b613378fdfb742a0df
jakub <kdoch@o2.pl>
2024-05-23 10:18:21 +0200

    Use Dto in save endpoint

--- src/main/java/com/example/springcrudapp/service/CompanyService.java
+++ src/main/java/com/example/springcrudapp/service/CompanyService.java
@@ -1,6 +1,7 @@
 package com.example.springcrudapp.service;
 
 import com.example.springcrudapp.model.*;
+import com.example.springcrudapp.model.DTO.CompanyDto;
 import com.example.springcrudapp.repository.CompanyRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,7 @@ public class CompanyService {
         return companyRepository.findAll();
     }
 
-    public Company save(Company company) {
-        company.setId(null);
-        return companyRepository.save(company);
+    public Company save(CompanyDto companyDto) {
+        return companyRepository.save(new Company(companyDto));
     }
 }
