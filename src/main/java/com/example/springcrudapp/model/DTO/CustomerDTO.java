509976313886059046c5694dbadd28943890daa5
jakub <kdoch@o2.pl>
2024-05-23 16:17:11 +0200

    Use lombok instead of getters and setters

--- src/main/java/com/example/springcrudapp/model/DTO/CustomerDTO.java
+++ src/main/java/com/example/springcrudapp/model/DTO/CustomerDTO.java
@@ -1,34 +1,13 @@
 package com.example.springcrudapp.model.DTO;
 
 import com.example.springcrudapp.model.Address;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CustomerDTO {
     private String name;
     private String surname;
-
     private Address address;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
 }
