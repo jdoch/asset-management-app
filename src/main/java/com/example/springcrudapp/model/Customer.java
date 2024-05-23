51cfa141b4df97a80ab6b4533f033c482e113843
jakub <kdoch@o2.pl>
2024-05-23 12:11:58 +0200

    Add DTO constructor

--- src/main/java/com/example/springcrudapp/model/Customer.java
+++ src/main/java/com/example/springcrudapp/model/Customer.java
@@ -1,11 +1,11 @@
 package com.example.springcrudapp.model;
 
+import com.example.springcrudapp.model.DTO.CustomerDTO;
 import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -20,11 +20,19 @@ public class Customer {
     private String name;
     private String surname;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     private Address address;
 
     @ManyToMany
     @JsonIgnoreProperties({"customers", "assets", "address"})
     private Set<Company> companies = new HashSet<>();
+
+    public Customer() {
+    }
+
+    public Customer(CustomerDTO customerDTO) {
+        name = customerDTO.getName();
+        surname = customerDTO.getSurname();
+        address = customerDTO.getAddress();;
+    }
 }
