664bb0bb4209ea94f4676dec43879277e61ca025
jakub <kdoch@o2.pl>
2024-05-23 16:56:30 +0200

    Add asset dto

---
+++ src/main/java/com/example/springcrudapp/model/DTO/AssetDTO.java
@@ -0,0 +1,11 @@
+package com.example.springcrudapp.model.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssetDTO {
+    private String name;
+    private double value;
+}
