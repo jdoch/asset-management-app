3777d5700a58f8e476d32139cf8eeb61127e8b2c
jakub <kdoch@o2.pl>
2024-05-23 09:36:51 +0200

    Create Address DTO

---
+++ src/main/java/com/example/springcrudapp/model/DTO/AddressDto.java
@@ -0,0 +1,42 @@
+package com.example.springcrudapp.model.DTO;
+
+import java.util.Objects;
+
+public class AddressDto {
+    private String street;
+    private String postalCode;
+    private String buildingNumber;
+    private String houseNumber;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+}
