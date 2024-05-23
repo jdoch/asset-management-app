c9db66d107fd5aaffccf8d82533cdb421da4cab7
jakub <kdoch@o2.pl>
2024-05-23 09:29:16 +0200

    Remove unused service method

--- src/main/java/com/example/springcrudapp/service/AssetService.java
+++ src/main/java/com/example/springcrudapp/service/AssetService.java
@@ -3,9 +3,6 @@ package com.example.springcrudapp.service;
 import com.example.springcrudapp.model.Asset;
 import com.example.springcrudapp.repository.AssetRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
 
 @Service
 public class AssetService {
@@ -16,22 +13,7 @@ public class AssetService {
         this.assetRepository = assetRepository;
     }
 
-    //@Transactional
-    @Transactional//(isolation = Isolation.READ_COMMITTED)
-    public Asset decreaseAmount(String id) {
-        var asset = assetRepository.findById(UUID.fromString(id)).orElseThrow();
-
-        if (asset.getValue() > 0) {
-            asset.setValue(asset.getValue() - 1);
-            System.out.println("Will be updated");
-        } else {
-            System.out.println("Will not be updated");
-        }
-        /*try {
-            Thread.sleep(30 * 1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-        return asset;
+    public Iterable<Asset> getAll() {
+        return assetRepository.findAll();
     }
 }
