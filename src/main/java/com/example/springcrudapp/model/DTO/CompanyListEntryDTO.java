c9e37da30ae7fa172b754fb96e987b6efe9fef08
jakub <kdoch@o2.pl>
2024-06-18 16:07:57 +0200

    Change id type to uuid

--- src/main/java/com/example/springcrudapp/model/DTO/CompanyListEntryDTO.java
+++ src/main/java/com/example/springcrudapp/model/DTO/CompanyListEntryDTO.java
@@ -4,14 +4,16 @@ import com.example.springcrudapp.model.Company;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 public class CompanyListEntryDTO {
-    private String id;
+    private UUID id;
     private String name;
 
     public CompanyListEntryDTO(Company company) {
-        id = company.getId().toString();
+        id = company.getId();
         name = company.getName();
     }
 }
