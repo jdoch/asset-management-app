f4713deba594fee9db1e85dab24f21c8f4d53f79
jakub <kdoch@o2.pl>
2024-06-19 18:28:23 +0200

    List companies using service

--- frontend/src/app/company-list/company-list.component.ts
+++ frontend/src/app/company-list/company-list.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatList, MatListItem} from "@angular/material/list";
 import {MatIcon} from "@angular/material/icon";
 import {MatFabButton} from "@angular/material/button";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatDivider} from "@angular/material/divider";
+import {CompanyDto} from "../model/company-dto";
+import {NgForOf} from "@angular/common";
+import {CompanyService} from "../service/company.service";
 
 @Component({
   selector: 'app-company-list',
@@ -14,11 +17,18 @@ import {MatDivider} from "@angular/material/divider";
     MatIcon,
     MatFabButton,
     MatPaginator,
-    MatDivider
+    MatDivider,
+    NgForOf
   ],
   templateUrl: './company-list.component.html',
   styleUrl: './company-list.component.css'
 })
-export class CompanyListComponent {
+export class CompanyListComponent implements OnInit {
+  companies: CompanyDto[] = [];
 
+  constructor(private companyService: CompanyService) { }
+
+  ngOnInit(): void {
+    this.companies = this.companyService.getAllCompanies();
+  }
 }
---
+++ frontend/src/app/service/company.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {CompanyDto} from "../model/company-dto";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  constructor() { }
+
+  getAllCompanies(): CompanyDto[] {
+    return [{ id: '12345', name: 'Company A'}, { id: '12345', name: 'Company B'}];
+  }
+}
