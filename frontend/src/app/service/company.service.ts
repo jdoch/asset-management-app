f52570fbeb45c6c09f989eeb1ce87f36927e9c94
jakub <kdoch@o2.pl>
2024-06-19 19:38:21 +0200

    Send request to backend

--- frontend/src/app/service/company.service.ts
+++ frontend/src/app/service/company.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import {CompanyDto} from "../model/company-dto";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  private apiUrl = 'localhost:8080/company';
+  constructor(private http: HttpClient) { }
 
   getAllCompanies(): CompanyDto[] {
     return [{ id: '12345', name: 'Company A'}, { id: '12345', name: 'Company B'}];
   }
 
-  getAmountOfCompanies(): number {
-    return 100;
+  getAmountOfCompanies(): Observable<number> {
+    return this.http.get<number>(this.apiUrl + '/count');
   }
 }
