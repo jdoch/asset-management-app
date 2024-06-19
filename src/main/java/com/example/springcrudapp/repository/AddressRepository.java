7c13ba1c77128e3c4835f59a1deb61de5796d158
jakub <kdoch@o2.pl>
2024-06-19 15:10:12 +0200

    move spring project to backend folder

--- src/main/java/com/example/springcrudapp/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springcrudapp.repository;
-
-import com.example.springcrudapp.model.Address;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface AddressRepository extends CrudRepository<Address, UUID> {
-}
